0b8d62b21319320079bae604e542ecc4aaa0f096
Bazinga0o0 <108140308+Bazinga0o0@users.noreply.github.com>
2024-01-18 18:36:11 +0100

    updated label disappearing

--- lqrscanner.py
+++ lqrscanner.py
@@ -1,22 +1,21 @@
 from PyQt5.QtWidgets import QApplication, QLabel
-from PyQt5.QtCore import Qt, QUrl
-from PyQt5.QtGui import QDesktopServices
+from PyQt5.QtCore import Qt, QUrl, QTimer
+from PyQt5.QtGui import QDesktopServices, QCursor
 from pyzbar.pyzbar import decode
 import cv2
-import pyautogui
-import time
 from mss import mss
 import numpy as np
-from PyQt5.QtCore import QTimer
-from PyQt5.QtGui import QCursor
 
 class ClickableLabel(QLabel):
-    def __init__(self, text):
-        super().__init__(text)
+    def __init__(self, text, parent=None):
+        super(ClickableLabel, self).__init__(text, parent)
         self.text = text
+        self.setCursor(Qt.PointingHandCursor)
 
     def mousePressEvent(self, event):
         QDesktopServices.openUrl(QUrl(self.text))
+        self.close()
+        scanner.scan()
 
 app = QApplication([])
 
@@ -25,34 +24,49 @@ class Scanner:
         self.label = None
         self.timer = QTimer()
         self.timer.timeout.connect(self.scan)
-        self.timer.start(1000)  # take a screenshot every second
+        self.timer.start(500)
         self.sct = mss()
+        self.no_codes_counter = 0
+        
 
     def scan(self):
-        screenshot = self.sct.grab(self.sct.monitors[0])  # take a screenshot of the entire screen
+        screenshot = self.sct.grab(self.sct.monitors[0])
+        
         image = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGBA2GRAY)
-        codes = decode(image)
+        try:
+            codes = decode(image)
+        except:
+            codes = None
         if not codes:
+            self.no_codes_counter += 1
+            if self.no_codes_counter >= 6:
+                self.timer.start(2000)
             if self.label is not None:
                 self.label.close()
         else:
+            self.no_codes_counter = 0
+            self.timer.start(300)
             mouse_pos = QCursor.pos()
             closest_code = min(codes, key=lambda code: (code.polygon[1].x - mouse_pos.x())**2 + (code.polygon[1].y - mouse_pos.y())**2)
             data = closest_code.data.decode('utf-8')
-            botP = closest_code.polygon[1]
+            botP = closest_code.polygon[0]
+            height = closest_code.rect.height
+            gap = 10
             if self.label is not None:
-                if data == self.label.text and self.label.pos().x() == botP.x and self.label.pos().y() == botP.y:
-                    return
-                else:
+                if data != self.label.text or self.label.pos().x() != botP.x or self.label.pos().y() != botP.y + height + gap:
                     self.label.close()
-            
-            self.label = ClickableLabel(data)
-            self.label.setStyleSheet("background-color: transparent; color: red; font-size: 18px;")
-            self.label.move(botP.x, botP.y)
-            self.label.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.SplashScreen)
-            self.label.show()
+                    self.create_label(data, botP, height, gap)
+            else:
+                self.create_label(data, botP, height, gap)
 
-scanner = Scanner()  # make scanner global
+    def create_label(self, data, botP, height, gap):
+        self.label = ClickableLabel(data)
+        self.label.setStyleSheet("background-color: rgba(128, 128, 128, 128); color: white; font-size: 18px;")
+        self.label.move(botP.x, botP.y + height + gap)
+        self.label.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.SplashScreen)
+        self.label.show()
+
+scanner = Scanner()
 
 if __name__ == '__main__':
     app.exec_()
